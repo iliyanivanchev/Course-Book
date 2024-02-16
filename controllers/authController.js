836844664266d9189579cbb20152aeb5e367ffa8
Iliyan Ivanchev <157714078+iliyanivanchev@users.noreply.github.com>
2024-02-16 14:56:30 +0200

    Add isGuest guard for login page
    After successful login to prevent going to login page after typing in URL address

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const { isGuest } = require('../middlewares/authMiddlewares');
 const authService = require('../services/authService');
 const { getErrorMessage } = require('../utils/errorUtil');
 
@@ -19,11 +20,11 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.get('/login', (req, res) => {
+router.get('/login', isGuest, (req, res) => {
     res.render('auth/login');
 });
 
-router.post('/login', async (req, res) => {
+router.post('/login', isGuest, async (req, res) => {
     const loginData = req.body;
     
     try {
